a823eb796c20f84657af166110a0414871df2a41
Jefferson Alves <jeffersonalves.dev@gmail.com>
2023-04-16 01:02:15 -0300

    Update: inserted bitcoin endpoint

--- src/controllers/api/v1/coins.controller.ts
+++ src/controllers/api/v1/coins.controller.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
 import coinsModels from '../../../models/cryptoCoins.models.js';
 
-const getAll = async (req: Request, res: Response) => {
-  const coins = await coinsModels.getAll();
+const cryptocoins = async (req: Request, res: Response) => {
+  const coins = await coinsModels.getAllCryptoCoins();
   return res.status(200).json({ coins });
 };
 
-export default { getAll };
+const fiatcoins = async (req: Request, res: Response) => {
+  const coins = await coinsModels.getAllFiatCoins();
+  return res.status(200).json({ coins });
+};
+
+export default { cryptocoins, fiatcoins };
--- src/routes/api/v1/coins.routes.ts
+++ src/routes/api/v1/coins.routes.ts
@@ -3,6 +3,7 @@ import coinsController from '../../../controllers/api/v1/coins.controller.js';
 
 const routersCoins = Router();
 
-routersCoins.get('/all', coinsController.getAll);
+routersCoins.get('/cryptocoins', coinsController.cryptocoins);
+routersCoins.get('/fiatcoins', coinsController.fiatcoins);
 
 export default routersCoins;
